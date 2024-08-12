5ccf436fc2c5f36da286ee0f1606740d38767f52
simonacatrini <simoacatrini12@yahoo.com>
2024-08-12 18:53:13 +0300

    c4

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -9,28 +9,28 @@ const generatePagination = (currentPage, totalCount) => {
   let pages = [];
   if (highestPage <= 8) {
     pages = Array.from({ length: highestPage }, (_, i) => i + 1);
-  } else if (currentPage > 3 && currentPage < highestPage - 2) {
+  } else if (currentPage > 3 && currentPage < highestPage - 3) {
     pages = [1];
     if (currentPage > 4) {
       pages.push('...');
     }
     pages.push(
-      ...[
-        currentPage - 2,
-        currentPage - 1,
-        currentPage,
-        currentPage + 1,
-        currentPage + 2,
-        '...',
-        highestPage,
-      ]
+      currentPage - 2,
+      currentPage - 1,
+      currentPage,
+      currentPage + 1,
+      currentPage + 2
     );
-  } else if (currentPage >= highestPage - 2) {
+    if (currentPage < highestPage - 3) {
+      pages.push('...');
+    }
+    pages.push(highestPage);
+  } else if (currentPage >= highestPage - 3) {
     pages = [1];
     if (highestPage > 8) {
       pages.push('...');
     }
-    for (let i = highestPage - 4; i <= highestPage; i++) {
+    for (let i = highestPage - 5; i <= highestPage; i++) {
       pages.push(i);
     }
   } else {
@@ -39,13 +39,16 @@ const generatePagination = (currentPage, totalCount) => {
 
   let markup = '';
   for (let i = 0; i < pages.length; i++) {
-    markup += `<li data-page="${pages[i]}" class="page ${
-      pages[i] == currentPage ? 'active' : ''
-    }">${pages[i]}</li>`;
+    if (typeof pages[i] === 'number') {
+      markup += `<li data-page="${pages[i]}" class="page ${
+        pages[i] === currentPage ? 'active' : ''
+      }">${pages[i]}</li>`;
+    } else {
+      markup += `<li class="page">${pages[i]}</li>`;
+    }
   }
 
   pagesContainer.innerHTML = markup;
 };
 
 export { generatePagination };
-
