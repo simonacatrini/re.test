ec174f75b6a7ea8aac2e4e993762a1150cd9be97
simonacatrini <simoacatrini12@yahoo.com>
2024-08-13 16:29:41 +0300

    c3

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -1,16 +1,12 @@
 import { HITSPERPAGE, PAGES_UPPER_LIMIT } from '../js/discoveryapi';
-
 const pagesContainer = document.querySelector('.pages');
-
 const generatePagination = (currentPage, totalCount) => {
   const maxPages = Math.ceil(totalCount / HITSPERPAGE);
   const highestPage = Math.min(maxPages, PAGES_UPPER_LIMIT);
-
   let pages = [];
-
   if (highestPage <= 8) {
     pages = Array.from({ length: highestPage }, (_, i) => i + 1);
-  } else if (currentPage > 3 && currentPage < highestPage - 3) {
+  } else if (currentPage > 3 && currentPage < highestPage - 2) {
     pages = [1];
     if (currentPage > 4) {
       pages.push('...');
@@ -22,11 +18,11 @@ const generatePagination = (currentPage, totalCount) => {
       currentPage + 1,
       currentPage + 2
     );
-    if (currentPage + 3 < highestPage) {
+    if (currentPage + 2 < highestPage - 1) {
       pages.push('...');
     }
     pages.push(highestPage);
-  } else if (currentPage >= highestPage - 3) {
+  } else if (currentPage >= highestPage - 2) {
     pages = [1];
     if (currentPage > 4) {
       pages.push('...');
@@ -39,21 +35,14 @@ const generatePagination = (currentPage, totalCount) => {
   } else {
     pages = [1, 2, 3, 4, '...', highestPage];
   }
-
   let markup = '';
   for (let i = 0; i < pages.length; i++) {
-    if (pages[i] === '...') {
-      markup += `<li class="page dots">...</li>`;
-    } else {
-      markup += `<li data-page="${pages[i]}" class="page ${
-        pages[i] == currentPage ? 'active' : ''
-      }">${pages[i]}</li>`;
-    }
+    markup += `<li data-page="${pages[i]}" class="page ${
+      pages[i] == currentPage ? 'active' : ''
+    }">${pages[i]}</li>`;
   }
-
   pagesContainer.innerHTML = markup;
 };
-
 export { generatePagination };
 
 
