06605c85511356b2111ff77c4caf9f0562dd9430
simonacatrini <simoacatrini12@yahoo.com>
2024-08-13 17:46:19 +0300

    C1

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -1,4 +1,4 @@
-import { HITSPERPAGE, PAGES_UPPER_LIMIT } from '../js/discoveryapi';
+ import { HITSPERPAGE, PAGES_UPPER_LIMIT } from '../js/discoveryapi';
 const pagesContainer = document.querySelector('.pages');
 const generatePagination = (currentPage, totalCount) => {
   const maxPages = Math.ceil(totalCount / HITSPERPAGE);
@@ -6,34 +6,22 @@ const generatePagination = (currentPage, totalCount) => {
   let pages = [];
   if (highestPage <= 8) {
     pages = Array.from({ length: highestPage }, (_, i) => i + 1);
-  } else if (currentPage > 3 && currentPage < highestPage - 2) {
-    pages = [1];
-    if (currentPage > 4) {
-      pages.push('...');
-    }
-    pages.push(
-      currentPage - 2,
-      currentPage - 1,
-      currentPage,
-      currentPage + 1,
-      currentPage + 2
-    );
-    if (currentPage + 2 < highestPage - 1) {
-      pages.push('...');
-    }
-    pages.push(highestPage);
-  } else if (currentPage >= highestPage - 2) {
-    pages = [1];
-    if (currentPage > 4) {
-      pages.push('...');
-    }
-    for (let i = highestPage - 4; i <= highestPage; i++) {
-      if (i > 1) {
-        pages.push(i);
-      }
-    }
   } else {
-    pages = [1, 2, 3, 4, '...', highestPage];
+    if (currentPage <= 4) {
+      pages = [1, 2, 3, 4, 5, '...', highestPage];
+    } else if (currentPage >= highestPage - 3) {
+      pages = [1, '...', highestPage - 4, highestPage - 3, highestPage - 2, highestPage - 1, highestPage];
+    } else {
+      pages = [
+        1,
+        '...',
+        currentPage - 1,
+        currentPage,
+        currentPage + 1,
+        '...',
+        highestPage
+      ];
+    }
   }
   let markup = '';
   for (let i = 0; i < pages.length; i++) {
@@ -44,6 +32,21 @@ const generatePagination = (currentPage, totalCount) => {
   pagesContainer.innerHTML = markup;
 };
 export { generatePagination };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
