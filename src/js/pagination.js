24afbe0eacba5faa784680066b089e8c99dc61cb
simonacatrini <simoacatrini12@yahoo.com>
2024-08-13 16:06:01 +0300

    c

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -1,16 +1,19 @@
 import { HITSPERPAGE, PAGES_UPPER_LIMIT } from '../js/discoveryapi';
+
 const pagesContainer = document.querySelector('.pages');
+
 const generatePagination = (currentPage, totalCount) => {
   const maxPages = Math.ceil(totalCount / HITSPERPAGE);
   const highestPage = Math.min(maxPages, PAGES_UPPER_LIMIT);
+
   let pages = [];
+
   if (highestPage <= 8) {
+    // Afișează toate paginile dacă sunt 8 sau mai puține
     pages = Array.from({ length: highestPage }, (_, i) => i + 1);
-  } else if (currentPage > 3 && currentPage < highestPage - 2) {
-    pages = [1];
-    if (currentPage > 4) {
-      pages.push('...');
-    }
+  } else if (currentPage > 4 && currentPage < highestPage - 3) {
+    // Afișează paginile din mijloc
+    pages = [1, '...'];
     pages.push(
       currentPage - 2,
       currentPage - 1,
@@ -18,32 +21,37 @@ const generatePagination = (currentPage, totalCount) => {
       currentPage + 1,
       currentPage + 2
     );
-    if (currentPage + 2 < highestPage - 1) {
-      pages.push('...');
-    }
-    pages.push(highestPage);
-  } else if (currentPage >= highestPage - 2) {
-    pages = [1];
-    if (currentPage > 4) {
-      pages.push('...');
-    }
+    pages.push('...', highestPage);
+  } else if (currentPage >= highestPage - 3) {
+    // Afișează ultimele pagini fără să depășească pagina maximă
+    pages = [1, '...'];
     for (let i = highestPage - 4; i <= highestPage; i++) {
       if (i > 1) {
         pages.push(i);
       }
     }
   } else {
-    pages = [1, 2, 3, 4, '...', highestPage];
+    // Afișează primele pagini și ultimele pagini
+    pages = [1, 2, 3, 4, 5, '...', highestPage];
   }
+
+  // Generarea markup-ului
   let markup = '';
   for (let i = 0; i < pages.length; i++) {
-    markup += `<li data-page="${pages[i]}" class="page ${
-      pages[i] == currentPage ? 'active' : ''
-    }">${pages[i]}</li>`;
+    if (typeof pages[i] === 'number') {
+      markup += `<li data-page="${pages[i]}" class="page ${
+        pages[i] === currentPage ? 'active' : ''
+      }">${pages[i]}</li>`;
+    } else {
+      markup += `<li class="page">${pages[i]}</li>`;
+    }
   }
+
   pagesContainer.innerHTML = markup;
 };
+
 export { generatePagination };
+
 
 
 
